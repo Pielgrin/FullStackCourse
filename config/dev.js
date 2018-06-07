3e2f7f0f01abc2a8c5c9043e48eda856e631fd79
Pierre Pellegrin <piepellegrin@gmail.com>
2018-06-07 22:57:09 +0200

    - finish authentification - logout

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js don't commit this
+module.exports = {
+    googleClientID : '952721502927-6b0ha442hdjkk0kt76igs2ja7eprelan.apps.googleusercontent.com',
+    googleClientSecret : 'n2OgX63TL8YsVi32zQHOZOV2',
+    mongoURI : 'mongodb://pierre:password33@ds245680.mlab.com:45680/emaily-dev2',
+    cookieKey: 'idnzdcijndsijcnzifnsjkncijznfjncvejhv'
+}
